a0c8d36ef6316be2ef87272014919df2b54bb98d
constt <175602385+constttdev@users.noreply.github.com>
2026-03-18 16:10:32 +0100

    feat: icon feat: campfire recipe rm: old block mixin

--- src/main/java/de/constt/campfire_additions/Campfire_additions.java
+++ src/main/java/de/constt/campfire_additions/Campfire_additions.java
@@ -1,10 +1,27 @@
 package de.constt.campfire_additions;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.TagKey;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.Containers;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class Campfire_additions implements ModInitializer {
 
+    public static final String MOD_ID = "campfire_additions";
+
     @Override
     public void onInitialize() {
+        // Mod initialization - mixins handle the main logic
     }
 }
